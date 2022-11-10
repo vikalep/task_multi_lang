677362798b896bb67990620f3329dcf117ff6dcd
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-11-10 19:04:07 +0300

    update conftest file

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=en, help="Choose language")
+    parser.addoption('--language', action='store', default="es", help="Choose language")
 
 @pytest.fixture(scope="function")    
 def browser(request):
